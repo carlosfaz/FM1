034a38b314e09a3f32e7649b995ede521b074711
carlosfaz <cefj94@gmail.com>
2021-02-11 22:37:05 -0700

    c
    c

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,62 @@
-latex_elements = {
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# Additional stuff for the LaTeX preamble.
-    'preamble': ''' \usepackage{fontspec} \setmainfont{Times New Roman}
-    ''',
-    'inputenc': '',
-    'fncychap': '',
-    'utf8extra': '',
-    'times': '',
-    'babel': '\usepackage{polyglossia}',
-    'cmap': '',
-    'fontenc': '',
-    'releasename': '', }
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'aaa'
+copyright = 'bbb'
+author = 'ccc'
+
+# The full version, including alpha/beta/rc tags
+release = '2021'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.todo']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+# -- Options for LaTeX output -------------------------------------------------
+latex_engine = 'xelatex'
+latex_elements = {'preamble':r'\usepackage{physics}'}
+
+# -- Options for TODO output -------------------------------------------------
+todo_include_todos = True
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
