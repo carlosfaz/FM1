a8fba0041d6fccf5ff4059faa8673234393a0313
carlosfaz <cefj94@gmail.com>
2021-02-12 20:02:12 -0700

    c
    c

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -35,15 +35,6 @@ extensions = [
     'sphinx_rtd_theme',
 ]
 
-html_theme_options = {
-    'github_user': 'sympy',
-    'github_repo': 'sphinx-math-dollar',
-    'github_banner': True,
-    'logo_name': True,
-    'travis_button': True,
-    'show_related': True,
-}
-
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
