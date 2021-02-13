1d7026842933a3bb9598843626bd8c03de3f6014
carlosfaz <cefj94@gmail.com>
2021-02-12 19:38:30 -0700

    c
    c

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -31,6 +32,7 @@ extensions = [
     'sphinx_math_dollar',
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
+    'sphinx_rtd_theme',
 ]
 
 html_theme_options = {
@@ -57,7 +59,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'basic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
