89b6f329909f23fd0422a4ef00f7d2695c1614b6
carlosfaz <cefj94@gmail.com>
2021-02-15 22:28:20 -0700

    c
    c

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -50,7 +50,26 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'                           
+                                                                                 
+if not on_rtd:  # only import and set the theme if we're building docs locally   
+    import sphinx_rtd_theme                                                      
+    html_theme = 'sphinx_rtd_theme'                                              
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]                   
+    # Override default css to get a larger width for local build                 
+    def setup(app):                                                              
+        #app.add_javascript("custom.js")                                         
+        app.add_stylesheet('theme_overrides.css')                                
+else:                                                                            
+    # Override default css to get a larger width for ReadTheDoc build            
+    html_context = {                                                             
+        'css_files': [                                                           
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',       
+            '_static/theme_overrides.css',                                       
+        ],                                                                       
+    }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
